12b1906dfbbd1378c392c84c66ffabfbd8e26234
crazyCode2 <1178770858@qq.com>
2017-11-21 17:12:36 +0800

    change QR

--- src/pages/QR.vue
+++ src/pages/QR.vue
@@ -9,17 +9,41 @@
     </mt-header>
     <!-- 内容 -->
     <div id="qrCode">
-      <QRCode val="https://www.baidu.com/s?wd=123" :show="true" />
+      <QRCode :val="val" :show="true" />
     </div>
+    <!-- 按钮 -->
+    <mt-button type="primary" @click="changeUrl">修改url</mt-button>
   </div>
 </template>
 
 <script>
   import QRCode from '../components/QRCode.vue'
+  import { MessageBox } from 'mint-ui';
+
   export default {
     name: 'QR',
     components: {
       QRCode
+    },
+    data(){
+      return {
+        val:'https://www.baidu.com/s?wd=123'
+      }
+    },
+    // watch: {
+    //   val: {
+    //     handler: function (val, oldVal) {
+    //       console.log(val);
+    //     },
+    //     deep: true
+    //   }
+    // },
+    methods: {
+      changeUrl(){
+        MessageBox.prompt('请输入新的url').then(({ value, action }) => {
+          this.val = value;
+        });
+      }
     }
   }
 </script>
@@ -33,5 +57,10 @@
     left: 50%;
     top: 50%;
     margin: -100px 0 0 -100px;
+  }
+  .mint-button{
+    width: 80%;
+    margin: 20px auto;
+    display: block;
   }
 </style>
