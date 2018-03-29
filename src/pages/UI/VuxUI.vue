363863c5a37d402d0bf9e1921a2fb7e2b8592235
crazyCode2 <1178770858@qq.com>
2018-03-29 09:36:10 +0800

    多栏滚动

--- src/pages/UI/VuxUI.vue
+++ src/pages/UI/VuxUI.vue
@@ -8,11 +8,17 @@
       <x-input :placeholder="'标题'"></x-input>
       <x-textarea :value="content" :max="200" name="description" :placeholder="'提示'"></x-textarea>
     </group>
+    <!-- 两栏滚动 -->
+    <divider>两栏滚动</divider>
+    <popup-picker :title="title2" :data="list2" :columns="2" v-model="value2" show-name></popup-picker>
+    <!-- 三栏滚动 -->
+    <divider>三栏滚动</divider>
+    <popup-picker :title="title3" :data="list3" :columns="3" v-model="value3" show-name></popup-picker>
   </div>
 </template>
 
 <script>
-  import { XHeader, Group, XInput, XTextarea } from 'vux'
+  import { XHeader, Group, XInput, XTextarea, Divider, PopupPicker } from 'vux'
 
   export default {
     name: 'VuxUI',
@@ -21,10 +27,100 @@
       Group,
       XInput,
       XTextarea,
+      Divider,
+      PopupPicker,
     },
     data(){
       return {
-        content:'使用vux,需要配置webpack.base.conf.js文件'
+        content:'使用vux,需要配置webpack.base.conf.js文件',
+        // 两栏滚动
+        title2: '联动显示文字',
+        list2: [{
+          name: '中国',
+          value: 'china',
+          parent: 0
+        }, {
+          name: '美国',
+          value: 'USA',
+          parent: 0
+        }, {
+          name: '广东',
+          value: 'china001',
+          parent: 'china'
+        }, {
+          name: '广西',
+          value: 'china002',
+          parent: 'china'
+        }, {
+          name: '美国001',
+          value: 'usa001',
+          parent: 'USA'
+        }, {
+          name: '美国002',
+          value: 'usa002',
+          parent: 'USA'
+        }],
+        value2: [],
+        // 三栏滚动
+        title3: '联动显示文字',
+        list3: [{
+          name: '中国',
+          value: 'china',
+          parent: 0
+        }, {
+          name: '美国',
+          value: 'USA',
+          parent: 0
+        }, {
+          name: '广东',
+          value: 'china001',
+          parent: 'china'
+        }, {
+          name: '广西',
+          value: 'china002',
+          parent: 'china'
+        }, {
+          name: '美国001',
+          value: 'usa001',
+          parent: 'USA'
+        }, {
+          name: '美国002',
+          value: 'usa002',
+          parent: 'USA'
+        }, {
+          name: '广州',
+          value: 'gz',
+          parent: 'china001'
+        }, {
+          name: '深圳',
+          value: 'sz',
+          parent: 'china001'
+        }, {
+          name: '广西001',
+          value: 'gx001',
+          parent: 'china002'
+        }, {
+          name: '广西002',
+          value: 'gx002',
+          parent: 'china002'
+        }, {
+          name: '美国001_001',
+          value: '0003',
+          parent: 'usa001'
+        }, {
+          name: '美国001_002',
+          value: '0004',
+          parent: 'usa001'
+        }, {
+          name: '美国002_001',
+          value: '0005',
+          parent: 'usa002'
+        }, {
+          name: '美国002_002',
+          value: '0006',
+          parent: 'usa002'
+        }],
+        value3: []
       }
     }
   }
